0f722da07277418dc5d1e12d51c99eefdc76fe6c
Rupesh12 <rupeshsoni001@gmail.com>
2016-08-09 19:28:44 -0400

    Bind Array Adapeter to list View and Add Referesh buttoon for debugging

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -4,13 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,47 +29,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
 
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            ArrayList<String> fake_data = new ArrayList<String>() ;
-            fake_data.add("Today-Sunny-88/63");
-            fake_data.add("Tommorow-Foggy-70/46");
-            fake_data.add("Wednesday-Rainy-90/90");
-            fake_data.add("Thursday-Rainy-90/90");
-            fake_data.add("Friday-Rainy-90/90");
-            fake_data.add("Saturday-Rainy-90/90");
-
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                    //the context (this, fragments parent activity)
-                    getActivity(),
-                    // ID of list item layout
-                    R.layout.list_item_forecast,
-                    //ID of  textview to fill up the data
-                    R.id.list_item_forecast_textview,
-                    //fake data to be populated
-                    fake_data);
-
-            return rootView;
-        }
-
-
-
-    }
 
 }
