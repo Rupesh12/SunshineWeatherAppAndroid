e9a465f25404837908b730245bf35e3aa821579c
Rupesh12 <rupeshsoni001@gmail.com>
2016-08-10 15:35:18 -0400

    modify doInBackground() to return the real time weather data

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -13,6 +13,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.text.format.Time;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,6 +25,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
@@ -99,12 +106,106 @@ import java.util.ArrayList;
         /////////////////////////
 
 
-        public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
+        public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
             private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+////////////////////////
+            private String getReadableDateString(long time){
+                SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+                return shortenedDateFormat.format(time);
+            }
 
+            private String formatHighLows(double high, double low) {
+                // For presentation, assume the user doesn't care about tenths of a degree.
+                long roundedHigh = Math.round(high);
+                long roundedLow = Math.round(low);
+
+                String highLowStr = roundedHigh + "/" + roundedLow;
+                return highLowStr;
+            }
+
+
+            private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+                            throws JSONException
+
+            {
+
+                                    // These are the names of the JSON objects that need to be extracted.
+                 final String OWM_LIST = "list";
+                 final String OWM_WEATHER = "weather";
+                 final String OWM_TEMPERATURE = "temp";
+                 final String OWM_MAX = "max";
+                 final String OWM_MIN = "min";
+                 final String OWM_DESCRIPTION = "description";
+                JSONObject forecastJson = new JSONObject(forecastJsonStr);
+                JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+                Time dayTime = new Time();
+                dayTime.setToNow();
+
+                int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+                dayTime = new Time();
+
+                String[] resultStrs = new String[numDays];
+                for(int i = 0; i < weatherArray.length(); i++) {
+                    // For now, using the format "Day, description, hi/low"
+                    String day;
+                    String description;
+                    String highAndLow;
+
+                    // Get the JSON object representing the day
+                    JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                    // The date/time is returned as a long.  We need to convert that
+                    // into something human-readable, since most people won't read "1400356800" as
+                    // "this saturday".
+                    long dateTime;
+                    // Cheating to convert this to UTC time, which is what we want anyhow
+                    dateTime = dayTime.setJulianDay(julianStartDay+i);
+                    day = getReadableDateString(dateTime);
+
+                    // description is in a child array called "weather", which is 1 element long.
+                    JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                    description = weatherObject.getString(OWM_DESCRIPTION);
+
+                    // Temperatures are in a child object called "temp".  Try not to name variables
+                    // "temp" when working with temperature.  It confuses everybody.
+                    JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                    double high = temperatureObject.getDouble(OWM_MAX);
+                    double low = temperatureObject.getDouble(OWM_MIN);
+
+                    highAndLow = formatHighLows(high, low);
+                    description = toCamelCase(description);
+                    resultStrs[i] = day + " - " + description + " - " + highAndLow;
+                }
+
+                for (String s : resultStrs) {
+                    Log.v(LOG_TAG, "Forecast entry: " + s);
+                }
+                return resultStrs;
+
+
+
+            }
+
+            private String toCamelCase(String s){
+                String[] parts = s.split(" ");
+                String camelCaseString = "";
+                for (String part : parts){
+                    camelCaseString = camelCaseString + toProperCase(part)+" ";
+                }
+                return camelCaseString;
+            }
+
+            private String toProperCase(String s) {
+                return s.substring(0, 1).toUpperCase() +
+                        s.substring(1).toLowerCase();
+            }
+
+            ////////////////////
             @Override
-            protected Void doInBackground(String... params) {
+            protected String[] doInBackground(String... params) {
 
                // verify that zip code has been provided
                 if(params.length == 0){
@@ -177,24 +278,21 @@ import java.util.ArrayList;
                         return null;
                     }
                     forecastJsonStr = buffer.toString();
+
                     Log.v(LOG_TAG,"ForeCast JSON String "+forecastJsonStr);
                 } catch (IOException e) {
                     Log.e(LOG_TAG, "Error ", e);
                     // If the code didn't successfully get the weather data, there's no point in attemping
                     // to parse it.
-                    return null;
-                } finally {
-                    if (urlConnection != null) {
-                        urlConnection.disconnect();
-                    }
-                    if (reader != null) {
-                        try {
-                            reader.close();
-                        } catch (final IOException e) {
-                            Log.e(LOG_TAG, "Error closing stream", e);
-                        }
-                    }
+
                 }
+                try {
+                    return getWeatherDataFromJson(forecastJsonStr, numDays);
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, e.getMessage(), e);
+                    e.printStackTrace();
+                }
+
                 return null;
             }
         }
